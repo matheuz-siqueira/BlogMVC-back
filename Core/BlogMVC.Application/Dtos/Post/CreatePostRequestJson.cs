57db2cf1f6959089ef1f78873aa6b94d4dc13f52
matheuz-siqueira <matheusbatista.dev@gmail.com>
2024-01-23 12:06:04 -0300

    feat: Atualizando regra de negócio para adicionar comentário

--- Core/BlogMVC.Application/Dtos/Post/CreatePostRequestJson.cs
+++ Core/BlogMVC.Application/Dtos/Post/CreatePostRequestJson.cs
@@ -1,12 +1,8 @@
-using BlogMVC.Application.Dtos.Comment;
-using BlogMVC.Application.Dtos.Tags;
-
 namespace BlogMVC.Application.Dtos.Post;
 
 public class CreatePostRequestJson 
 {
     public string Title { get; set; }
     public string Subtitle { get; set; }
-    public string Content { get; set; }
-    public IList<CreateTagRequestJson> Tags { get; set; }
+    public string Content { get; set; } 
 }
